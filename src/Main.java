363ed160a322c5510e4008de072ada0a46d8062f
Puzyrnikow Andrey <daf1>
2023-02-21 23:24:42 +0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        WordsChecker wordsChecker=new WordsChecker("Lorem ipsum dolor sit amet, consectetur adipiscing elit");
-        System.out.println(wordsChecker.hasWord("dolor"));
-        System.out.println(wordsChecker.hasWord("ol"));
+        WordsChecker wordsChecker = new WordsChecker();
+        System.out.println(wordsChecker.hasWord("mollit"));
         System.out.println(wordsChecker.set);
     }
 }
--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -2,11 +2,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class WordsChecker {
-    String text;
-    Set<String> set = new HashSet<>();
+    protected static String text = "Lorem ipsum dolor sit amet," +
+            " consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+            " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+            " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+            " Excepteur sint occaecat cupidatat non proident," +
+            " sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    protected static Set<String> set = new HashSet<>();
 
-    public WordsChecker(String text) {
+    public WordsChecker(String text, Set<String> set) {
         this.text = text;
+        this.set = set;
+    }
+
+    public WordsChecker() {
+        this(text, set);
     }
 
     boolean hasWord(String word) {
